5d29388f1644d44d009f50282db2be5244cc67ae
everthis <wwwaap@gmail.com>
2015-11-18 23:14:37 +0800

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,16 +79,13 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.qq.com",
+   :port                 => 587,
+   :user_name            => "1147972301@qq.com",
+   :password             => "83538107!!!",
+   :authentication       => "plain",
+  :enable_starttls_auto => true
   }
   
 end
