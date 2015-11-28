72b9a281b33c3996c93ab37af8d3c6aa4268fb9e
everthis <wwwaap@gmail.com>
2015-11-28 21:11:17 +0800

    modify user-microposts branch.

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,42 +2,6 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   before_action :correct_user,   only: :destroy
 
-  # GET /microposts
-  # GET /microposts.json
-  def index
-    @microposts = Micropost.all
-  end
-
-  # GET /microposts/1
-  # GET /microposts/1.json
-  def show
-  end
-
-  # GET /microposts/new
-  def new
-    @micropost = Micropost.new
-  end
-
-  # GET /microposts/1/edit
-  def edit
-  end
-
-  # POST /microposts
-  # POST /microposts.json
-  # def create
-  #   @micropost = Micropost.new(micropost_params)
-
-  #   respond_to do |format|
-  #     if @micropost.save
-  #       format.html { redirect_to @micropost, notice: 'Micropost was successfully created.' }
-  #       format.json { render :show, status: :created, location: @micropost }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @micropost.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
@@ -48,10 +12,7 @@ class MicropostsController < ApplicationController
       render 'static_pages/home'
     end
   end 
-  
 
-  # PATCH/PUT /microposts/1
-  # PATCH/PUT /microposts/1.json
   def update
     respond_to do |format|
       if @micropost.update(micropost_params)
@@ -63,16 +24,6 @@ class MicropostsController < ApplicationController
       end
     end
   end
-
-  # DELETE /microposts/1
-  # DELETE /microposts/1.json
-  # def destroy
-  #   @micropost.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to microposts_url, notice: 'Micropost was successfully destroyed.' }
-  #     format.json { head :no_content }
-  #   end
-  # end
 
   def destroy
     @micropost.destroy
