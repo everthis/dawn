be31be4e505bfc8c4dc26e6b8b5f3e4269ae7e30
everthis <wwwaap@gmail.com>
2015-11-30 23:30:26 +0800

    fix test error.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
-  root             'static_pages#home'
+  root               'static_pages#home'
   get 'help'      => 'static_pages#help'
   get 'about'     => 'static_pages#about'
   get 'contact'   => 'static_pages#contact'
