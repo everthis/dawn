8b4253faae0dd5d16753b67e307424565b10482d
everthis <wwwaap@gmail.com>
2015-12-02 23:51:14 +0800

    modify test case.

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,49 +1,31 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionController::TestCase
-  setup do
-    @micropost = microposts(:one)
+  def setup
+    @micropost = microposts(:orange)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:microposts)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create micropost" do
-    assert_difference('Micropost.count') do
-      post :create, micropost: { content: @micropost.content, user_id: @micropost.user_id }
+  test "should redirect create when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post :create, micropost: { content: "Lorem ipsum" }
     end
-
-    assert_redirected_to micropost_path(assigns(:micropost))
+    assert_redirected_to login_url
   end
 
-  test "should show micropost" do
-    get :show, id: @micropost
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @micropost
-    assert_response :success
-  end
-
-  test "should update micropost" do
-    patch :update, id: @micropost, micropost: { content: @micropost.content, user_id: @micropost.user_id }
-    assert_redirected_to micropost_path(assigns(:micropost))
-  end
-
-  test "should destroy micropost" do
-    assert_difference('Micropost.count', -1) do
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'Micropost.count' do
       delete :destroy, id: @micropost
     end
-
-    assert_redirected_to microposts_path
+    assert_redirected_to login_url
   end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, id: micropost
+    end
+    assert_redirected_to root_url
+  end
+  
 end
