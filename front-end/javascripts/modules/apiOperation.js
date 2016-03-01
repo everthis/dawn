cdd25581c00228c9b691c8613925c8df8e2cab91
everthis <wwwaap@gmail.com>
2016-03-01 22:08:49 +0800

    code of front-end of API addition has been accomplished.

--- front-end/javascripts/modules/apiOperation.js
+++ front-end/javascripts/modules/apiOperation.js
@@ -9,8 +9,7 @@ let apisArr = [];
 
 var callback = {
   getApiSuccess: function(data) {
-    let newApi = new ApiDom(JSON.parse(data), this);
-    apisArr.push(newApi);
+    addApiTree(JSON.parse(data), this);
   },
   getAllApisSuccess: function(data) {
     renderAllApis(data);
@@ -34,33 +33,70 @@ export function initXhr() {
 function toggleFoldLi(context) {
   context.classList.toggle('unfold');
 }
+function bindEventToApiLiDescription(ev) {
+  toggleFoldLi(this);
+  if (this.nextElementSibling) {
+    return;
+  };
+  $http(rootAPI + '/' + this.parentNode.dataset.apiId)
+  .get(payload)
+  .then(callback.getApiSuccess.bind(this.parentNode))
+  .catch(callback.error);
+}
 function bindevents() {
   let apiLis = document.getElementsByClassName('api-li-description');
   [].slice.call(apiLis).forEach(function(element, index) {
     element.addEventListener('click', function(ev) {
-      toggleFoldLi(this);
-      if (this.nextElementSibling) {
-        return;
-      };
-      $http(rootAPI + '/' + this.parentNode.dataset.apiId)
-      .get(payload)
-      .then(callback.getApiSuccess.bind(this.parentNode))
-      .catch(callback.error);
+      bindEventToApiLiDescription.call(this, ev);
     });
   });
+}
+function addApiTree(data = {}, containerNode) {
+  let newApi = new ApiDom(data, containerNode);
+  apisArr.push(newApi);
+}
+function newApiBtn() {
+  let newApiDiv = document.createElement('div');
+  let header = document.getElementsByTagName('header')[0];
+  newApiDiv.classList.add('new-api');
+  newApiDiv.innerHTML = `<input class="add-api-btn" type="button" value="new API">`;
+  newApiDiv.addEventListener('click', function() {
+    let apiUl = document.getElementsByClassName('api-ul')[0];
+    let baseApiLi = strToDom(newApiLiTpl());
+    apiUl.insertBefore(baseApiLi, apiUl.firstChild);
+    addApiTree({}, baseApiLi);
+    toggleFoldLi(baseApiLi.children[0]);
+    baseApiLi.children[0].addEventListener('click', function(ev) {
+      bindEventToApiLiDescription.call(this, ev);
+    });
+  });
+  insertAfter(newApiDiv, header);
+  return newApiDiv;
+}
+function strToDom(str) {
+  let tmpEle = document.createElement('div');
+  tmpEle.innerHTML = str;
+  let returnDom = tmpEle.children[0];
+  return returnDom;
+}
+function newApiLiTpl(data = {}) {
+  var tpl = `
+    <li class="api-li" data-api-id="${data.id || null}">
+      <div class="api-li-description">
+        <span class="api-li-collapse"><svg class="icon icon-down"><use xlink:href="#icon-down"></use></svg></span>
+        <span class="api-li-uri">${data.uri || '(No uri)'}</span>
+        <span class="api-li-name">${data.name ? data.name : '(No name)'}</span>
+      </div>
+    </li>
+  `;
+  return tpl;
 }
 function renderAllApis(data) {
   data = JSON.parse(data);
   const tmpl = data => html`
       <ul class="api-ul">
       ${data.map(item => html`
-          <li class="api-li" data-api-id="$${item.id}">
-            <div class="api-li-description">
-              <span class="api-li-collapse"><svg class="icon icon-down"><use xlink:href="#icon-down"></use></svg></span>
-              <span class="api-li-uri">$${item.uri || "(No uri)"}</span>
-              <span class="api-li-name">$${item.name ? item.name : "(No name)"}</span>
-            </div>
-          </li>
+        ${newApiLiTpl(item)}
       `)}
       </ul>
   `;
@@ -68,7 +104,7 @@ function renderAllApis(data) {
   let apiListEle = document.createElement('div');
   apiListEle.classList.add('api-ul-wrapper');
   apiListEle.innerHTML = tmpl(data);
-  insertAfter(apiListEle, header);
+  insertAfter(apiListEle, newApiBtn());
 }
 function getAllApis() {
   $http(rootAPI)
@@ -76,8 +112,6 @@ function getAllApis() {
   .then(callback.getAllApisSuccess)
   .catch(callback.error);
 }
-
-function collapse() {}
 
 function bindEvent(ev) {
   if (ev.target.classList.contains('api-save')) {
