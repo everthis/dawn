cf6f2f6f19775e2cfe75ff363853b26d8f6db0d5
everthis <wwwaap@gmail.com>
2016-05-03 13:31:28 +0800

    fix CI errors.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,27 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #  :address              => "smtp.qq.com",
+  #  :port                 => 587,
+  #  :user_name            => "",
+  #  :password             => "",
+  #  :authentication       => "plain",
+  #  :enable_starttls_auto => true
+  # }
+  host = ENV["RAILS_HOST"]
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
-   :address              => "smtp.qq.com",
-   :port                 => 587,
-   :user_name            => "",
-   :password             => "",
-   :authentication       => "plain",
-  :enable_starttls_auto => true
+    address: "smtp.exmail.qq.com",
+    port: 25,
+    domain: "qq.com",
+    authentication: "plain", 
+    user_name: ENV["RAILS_EMAIL_FROM"], 
+    password: ENV["RAILS_EMAIL_PWD"], 
+    enable_starttls_auto: true
   }
-  
+
 end
