9e32a7da4621034143a00616d926dc67e8109af7
everthis <wwwaap@gmail.com>
2016-06-11 11:17:49 +0800

    try to fix env var missing error.

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,8 +45,11 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 set :rails_env,       "production"
 
+# capistrano-puma
+set :puma_conf,       "config/puma.rb"
+
 # Don't change these unless you know what you're doing
-set :pty,             false
+set :pty,             true
 set :use_sudo,        false
 set :stage,           :production
 # set :deploy_via,      :remote_cache
