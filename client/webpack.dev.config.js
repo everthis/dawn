15d09252d536fa6eea5ecdb325705ee3578ce05c
everthis <wwwaap@gmail.com>
2016-11-29 21:52:50 +0800

    add fis-conf.js.

--- client/webpack.dev.config.js
+++ client/webpack.dev.config.js
@@ -1,4 +1,5 @@
 var merge = require('webpack-merge');
+let webpack = require('webpack');
 var baseConfig = require('./webpack.base.config').defaults;
 var devConfig = {};
 let serverConfig = {
@@ -10,22 +11,26 @@ let serverConfig = {
 let devServerOrigin = '' + serverConfig.protocol + serverConfig.host + ':' + serverConfig.port;
 let devServerHref = '' + devServerOrigin + '/assets/';
 let devServerEntry = 'webpack-dev-server/client?' + devServerOrigin;
-// for (var name in baseConfig.entry) {
-//     if (baseConfig.entry.hasOwnProperty(name)) {
-//       baseConfig.entry[name].unshift(devServerEntry)
-//     }
-// };
+for (var name in baseConfig.entry) {
+    if (baseConfig.entry.hasOwnProperty(name)) {
+      baseConfig.entry[name].unshift(devServerEntry)
+    }
+};
 var cl = console.log;
 devConfig = merge.smartStrategy({
     'entry': 'prepend',
     'module.loaders': 'prepend'
 })({}, baseConfig, {
-    // output: {
-    //     publicPath: devServerHref
-    // },
+    output: {
+        publicPath: '/assets/'
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+      // activates HMR
+    ]
     // devServer: {
     //     host: '0.0.0.0',
-    //     port: 8679,
+    //     port: 8679
     // }
 });
 module.exports = devConfig;
