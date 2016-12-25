ac8d1c489dd01a34d689a0012e2a5ba7db84d671
hejie03 <hejie03@baidu.com>
2016-12-25 20:49:25 +0800

    add package_name

--- app/jobs/process_fis_ci_plugins_job.rb
+++ app/jobs/process_fis_ci_plugins_job.rb
@@ -5,9 +5,10 @@ class ProcessFisCiPluginsJob < ApplicationJob
   	@content = JSON.parse FisCiPlugin.find(id).content
   	@content.each { |item|
   		user_input = item['input']
+      package_name = item['package']
   		version = item['version']
   		ci_package_fullname = "#{item['ciPackageNamePrefix']}#{item['ciPackageName']}"
-  		`bash /home/users/hejie03/idev-projects/fis-ci-plugins/download_rename_publish.sh #{user_input} #{ci_package_fullname}`
+  		`bash /home/users/hejie03/idev-projects/fis-ci-plugins/download_rename_publish.sh #{user_input} #{ci_package_fullname} #{package_name}`
   		puts "package #{ci_package_fullname}@#{version} published"
   		`node /home/users/hejie03/idev-projects/fis-ci-plugins/save_request.js #{ci_package_fullname} #{version}`
   		puts "package is being installed on FIS CI machines. :)"
