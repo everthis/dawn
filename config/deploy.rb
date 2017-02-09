35943f08449ff7da508c1ce2e0b43749d7cc5af5
hejie03 <hejie03@baidu.com>
2017-02-09 10:44:38 +0800

    update lock version.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.6.1'
+lock '3.7.2'
 
 # set :application, 'my_app_name'
 # set :repo_url, 'git@example.com:me/my_repo.git'
@@ -77,7 +77,7 @@ set :sidekiq_log, File.join(shared_path, 'log', 'sidekiq.log')
 set :sidekiq_options, nil
 set :sidekiq_require, nil
 set :sidekiq_tag, nil
-set :sidekiq_config, nil # if you have a config/sidekiq.yml, do not forget to set this. 
+set :sidekiq_config, nil # if you have a config/sidekiq.yml, do not forget to set this.
 set :sidekiq_queue, ['default', 'mailers']
 set :sidekiq_timeout, 10
 set :sidekiq_role, :app
