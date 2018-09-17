11cc26d7473ebf4745272a43893f8166ae86b2c9
everthis <wwwaap@gmail.com>
2018-09-17 23:53:14 +0800

    fix title escape.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 require 'uri'
+require 'cgi'
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
@@ -6,7 +7,7 @@ module ApplicationHelper
     if page_title.empty?
       base_title
     else
-      page_title + " | " + base_title
+      CGI.unescapeHTML(page_title) + " | " + base_title
     end
   end
 
